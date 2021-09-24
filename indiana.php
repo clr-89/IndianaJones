936ecfde62708feab8088c12a0a143069f11555c
clr-89 <clr.89@gmail.com>
2021-09-24 22:39:55 +0200

    indiana jones variables en php

---
+++ indiana.php
@@ -0,0 +1,14 @@
+<?php
+
+$nameVoMovie = "Indiana Jones and the last crusade";
+echo $nameVoMovie;
+
+$userWatchMovie = false;
+echo $userWatchMovie;
+
+$yearMovie = 1989;
+echo $yearMovie;
+
+$userRatingsMovieOnImdb = 8.2;
+echo $userRatingsMovieOnImdb;
+
